f375c85562bebf68c71bd406a050eb0d5d4ac80b
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-01-08 12:03:46 +1100

    Extend Category with methods related to loading default categories

--- EntityExtensions/Category.swift
+++ EntityExtensions/Category.swift
@@ -5,4 +5,34 @@
 //  Created by Midori Verdouw on 6/1/22.
 //
 
-import Foundation
+import CoreData
+
+extension Category {
+    static func categoriesCount(context: NSManagedObjectContext) -> Int {
+        let request = fetchRequest(NSPredicate(format: "TRUEPREDICATE"))
+        let categories = (try? context.fetch(request)) ?? []
+        return categories.count
+    }
+    
+    static func fetchRequest(_ predicate: NSPredicate) -> NSFetchRequest<Category> {
+        let request = NSFetchRequest<Category>(entityName: "Category")
+        request.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+        request.predicate = predicate
+        return request
+    }
+    
+    static func add(name: String, id: Int, in context: NSManagedObjectContext) {
+        let category = Category(context: context)
+        category.name = name
+        category.id = Int16(id)
+        category.objectWillChange.send()
+        category.recipes.forEach { $0.objectWillChange.send() }
+        
+        try? context.save()
+    }
+    
+    var recipes: Set<Recipe> {
+        get { (recipes_ as? Set<Recipe>) ?? [] }
+        set { recipes_ = newValue as NSSet }
+    }
+ }
