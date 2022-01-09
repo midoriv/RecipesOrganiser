5e6d3bc81e41ead285cfb4df9eac336c3467af49
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-01-10 10:56:14 +1100

    Change the key path for sorting Categories from id to name_

--- EntityExtensions/Category.swift
+++ EntityExtensions/Category.swift
@@ -26,7 +26,7 @@ extension Category {
     
     static func fetchRequest(_ predicate: NSPredicate) -> NSFetchRequest<Category> {
         let request = NSFetchRequest<Category>(entityName: "Category")
-        request.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
+        request.sortDescriptors = [NSSortDescriptor(key: "name_", ascending: true)]
         request.predicate = predicate
         return request
     }
