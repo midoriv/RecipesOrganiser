108e45f13f001f51755457e4e38530bf5a9159d6
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-01-10 11:55:36 +1100

    Add a method to add a Recipe to CoreData

--- EntityExtensions/Recipe.swift
+++ EntityExtensions/Recipe.swift
@@ -14,4 +14,17 @@ extension Recipe {
         request.predicate = predicate
         return request
     }
+    
+    static func add(name: String, url: String, categoryName: String, in context: NSManagedObjectContext) {
+        let recipe = Recipe(context: context)
+        recipe.name = name
+        recipe.url = url
+        recipe.count = 0
+        recipe.timestamp = Date()
+        recipe.id = UUID()
+        recipe.objectWillChange.send()
+        recipe.category = Category.withName(categoryName, context: context)
+        try? context.save()
+        print("Recipe saved")
+    }
 }
