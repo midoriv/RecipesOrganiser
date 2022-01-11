d99e88efe619ce12fa592c62702da8a012109d0e
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-01-11 12:42:16 +1100

    Add methods to find and update recipes

--- EntityExtensions/Recipe.swift
+++ EntityExtensions/Recipe.swift
@@ -15,6 +15,26 @@ extension Recipe {
         return request
     }
     
+    // find a Recipe with a given id
+    static func withId(_ id: UUID, in context: NSManagedObjectContext) -> Recipe? {
+        let request = fetchRequest(NSPredicate(format: "id = %@", id as CVarArg))
+        let recipe = (try? context.fetch(request)) ?? []
+        return recipe.first
+    }
+    
+    static func update(id: UUID, name: String, url: String, categoryName: String, in context: NSManagedObjectContext) {
+        if let recipe = withId(id, in: context) {
+            recipe.name = name
+            recipe.url = url
+            recipe.timestamp = Date()
+            if let category = Category.withName(categoryName, context: context) {
+                recipe.category = category
+            }
+            try? context.save()
+            print("Recipe updated")
+        }
+    }
+    
     static func add(name: String, url: String, categoryName: String, in context: NSManagedObjectContext) {
         let recipe = Recipe(context: context)
         recipe.name = name
