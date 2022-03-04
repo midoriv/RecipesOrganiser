942b86a5ed620c2b3024eae4a47a1b776ab7c700
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-03-04 11:38:14 +1100

    Delete preview for RecipeRowView

--- RecipesOrganiser/Views/RecipeRowView.swift
+++ RecipesOrganiser/Views/RecipeRowView.swift
@@ -25,19 +25,3 @@ struct RecipeRowView: View {
         }
     }
 }
-
-struct RecipeRowView_Previews: PreviewProvider {
-    
-    static var previews: some View {
-        let context = PersistenceController.preview.container.viewContext
-        let recipe = Recipe(context: context)
-        let category = Category(context: context)
-        category.name = "Pasta"
-        
-        recipe.name = "Test Recipe"
-        recipe.category = category
-        
-        return RecipeRowView(recipe: recipe)
-            .previewLayout(.fixed(width: 400, height: 60))
-    }
-}
