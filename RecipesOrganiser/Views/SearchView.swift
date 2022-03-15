c788525176974be4928e7cf2fb7e4f73a882cd4a
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-03-15 14:26:57 +1100

    Add search functionality and result list view

--- RecipesOrganiser/Views/SearchView.swift
+++ RecipesOrganiser/Views/SearchView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct SearchView: View {
+    @FetchRequest(fetchRequest: Recipe.fetchRequest(NSPredicate(format: "TRUEPREDICATE"))) var recipes: FetchedResults<Recipe>
+    @FetchRequest(fetchRequest: Category.fetchRequest(NSPredicate(format: "TRUEPREDICATE"))) var categories: FetchedResults<Category>
+    @State private var searchText = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List{
+                ForEach(searchResults, id: \.self) { recipe in
+                    NavigationLink(destination: RecipeWebView(urlStr: recipe.url)) {
+                        RecipeRowView(recipe: recipe)
+                    }
+                }
+            }
+            .searchable(text: $searchText, prompt: "Search recipes")
+            .navigationTitle("Search")
+        }
+    }
+    
+    var searchResults: [Recipe] {
+        if searchText.isEmpty {
+            return []
+        }
+        else {
+            var results = [Recipe]()
+            results += recipes.filter { $0.name.localizedStandardContains(searchText) }
+
+            let filteredCategories = categories.filter { $0.name.localizedStandardContains(searchText) }
+            filteredCategories.forEach({ category in
+                category.recipes.forEach { recipe in
+                    if !results.contains(recipe) {
+                        results.append(recipe)
+                    }
+                }
+            })
+            return results
+        }
     }
 }
 
